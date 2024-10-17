2e70c9d8fa60ffe7b08ebfe9cdb899101d0e9292
Maxim Chavlytko <phobos1313@gmail.com>
2024-10-18 01:32:11 +0700

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
     implementation("com.h2database:h2:$h2Version")
 }
 
